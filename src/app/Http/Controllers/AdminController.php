9ad4bc225e692ded15f3be42a4dda9fe73612721
andmatcha <jj.mcr185@gmail.com>
2022-07-01 01:07:47 +0900

    並べ替え完成

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -23,7 +23,7 @@ class AdminController extends Controller
     public function edit($big_question_id)
     {
         if (Auth::check()) {
-            $big_question = BigQuestion::find($big_question_id)->load(['questions.choices' => function ($query) {
+            $big_question = BigQuestion::find($big_question_id)->load(['questions' => function ($query) {
                 $query->orderby('question_order', 'asc');
             }]);
             $data = [
