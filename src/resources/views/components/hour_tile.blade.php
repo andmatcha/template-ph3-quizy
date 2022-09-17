a0dbf95c109a56118918fb3cda489060d959b788
andmatcha <jj.mcr185@gmail.com>
2022-09-18 00:54:28 +0900

    コンポーネント化

---
+++ src/resources/views/components/hour_tile.blade.php
@@ -0,0 +1,5 @@
+<div class="main__container__area_hours__box main__container__area_tile">
+    <p class="main__container__area_hours__box__title">{{ $title }}</p>
+    <p class="main__container__area_hours__box__num">{{ $amount }}</p>
+    <p class="main__container__area_hours__box__unit">hours</p>
+</div>
